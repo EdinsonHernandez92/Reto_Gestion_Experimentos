aa171c0e1f4859ac558b512cf5c4c407ed8e15c2
EdinsonHernandez92 <edha92@gmail.com>
2024-11-24 19:51:46 -0500

    Create GestionExperimentos.py

---
+++ GestionExperimentos.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import statistics
+
+class Experimento:
+    # funcion de inicializacion = metodo constructor
+    def __init__(self, nombreExperimento, fecha, tipoExperimento, resultado):
+        self.nombreExperimento = nombreExperimento
+        self.fecha = fecha
+        self.tipoExperimento = tipoExperimento
+        self.resultado = resultado
+
+# funcion para agregar una experimento - PIPE
+def agregarExperimento(listaExperimentos):
+    pass
+
+# funcion para visualizar todos los experimentos - PIPE
+def visualizarExperimento(listaExperimentos):
+    pass
+
+# funcion para calculos basicos - EDINSON
+def calcularExperimento(listaExperimentos):
+    pass
+
+# funcion para comparar experimentos - EDINSON
+def compararExperimentoSeleccionados(listaExperimentos):
+    pass
+
+# Funcion para generar informes - PIPE
+def generarInforme(listaExperimentos):
+    pass
+
+# Menu - EDINSON
+def menu():
+    pass
+        
+if __name__ == "__main__":
+    menu()
